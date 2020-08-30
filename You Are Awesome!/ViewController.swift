4803b7ece50ab42a2064218067c1b9bff69e8468
Young Hur <huryo@bc.edu>
2020-08-30 13:16:39 -0400

    Initial Commit

---
+++ You Are Awesome!/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  You Are Awesome!
+//
+//  Created by yhur18 on 8/30/20.
+//  Copyright © 2020 Young Hur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
